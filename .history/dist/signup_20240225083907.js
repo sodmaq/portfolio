94cdd21cd34f10db50f7b01df38daca0431e4267
sodmaq <sodmaq@gmail.com>
2024-02-25 13:57:32 +0100

    made changes

---
+++ .history/dist/signup_20240225083907.js
@@ -0,0 +1,44 @@
+const apiUrl = 'https://natours-production-095c.up.railway.app/api/v1/users/signup';
+
+const signup = async (name, email, password, passwordConfirm, role) => {
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name, email, password, passwordConfirm, role })
+    });
+
+    const data = await response.json();
+
+    if (data.status === 'success') {
+      alert('Signup successful');
+      window.location.href('./')
+    } else {
+      alert('Signup failed: ' + data.message);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    alert('An error occurred. Please try again.');
+  }
+};
+
+const signupForm = document.getElementById("signupForm");
+
+signupForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const passwordConfirm = document.getElementById("passwordConfirm").value;
+  const role =document.getElementById("role").value
+
+  try {
+    await signup(name, email, password, passwordConfirm, role);
+  } catch (error) {
+    console.error(error);
+    // Handle error if needed
+  }
+});
