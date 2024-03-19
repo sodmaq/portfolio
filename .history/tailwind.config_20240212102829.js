99187812151ae0ecd6591ba55f3710bde3031937
sodmaq <sodmaq@gmail.com>
2024-03-19 06:55:17 +0100

    Removed .history folder

--- .history/tailwind.config_20240212102829.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'class'
-  content: ["./dist/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("flowbite/plugin")],
-};
