77c66076aa92315d73fceefcd4eba45ed614fa40
sodmaq <sodmaq@gmail.com>
2024-03-31 06:55:58 +0100

    updated CV

---
+++ .history/dist/login_20240225082110.js
@@ -0,0 +1,32 @@
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const email = document.getElementById("email").value;  // Change from "username" to "email"
+  const password = document.getElementById("password").value;
+  const apiUrl = "https://natours-production-095c.up.railway.app/api/v1/users/login";
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),  // Use "email" field instead of "username"
+    });
+
+    const data = await response.json();
+
+    if (data.status === "success") {  // Check for "success" status
+      alert("Login successful")
+      window.location('')
+      // Redirect to dashboard or do something else
+    } else {
+      alert("Login failed: " + data.message);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    alert("An error occurred. Please try again.");
+  }
+});
